b9f0e0acd570d239bee2acbff39563ed70a8b3cf
Vladimir Heredia <vheredia04@hotmail.com>
2019-02-22 23:37:23 -0500

    Comments added

--- src/app/shared/department.service.ts
+++ src/app/shared/department.service.ts
@@ -8,6 +8,11 @@ export class DepartmentService {
   departmentList: AngularFireList<any>;
   array = [];
 
+  /**
+   * This service get's the department's information for
+   * the dropdown
+   * @param firebase 
+   */
   constructor(private firebase: AngularFireDatabase) {
     this.departmentList = this.firebase.list('departments');
     this.departmentList.snapshotChanges().subscribe(
